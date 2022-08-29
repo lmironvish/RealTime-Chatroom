e31fc9502885983d77dc06ce2967ea98400b1b09
Lisa Kurdzhieva <lizakpost@gmail.com>
2022-08-29 13:42:46 +0300

    feat: base realtime chatroom template

---
+++ README.md
@@ -0,0 +1,28 @@
+# Learn JavaScript
+
+---
+
+## Concepts
+
+- single thread non blocking
+
+- execution context
+- global execution context
+- function execution context
+
+- lexical environment
+- variable environment
+
+- this
+
+- global memory
+- call stack
+- LIFO (last in first out)
+
+- heap (memory allocation)
+
+- queue
+- callback queue
+- event loop
+
+- web api
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+const path = require("path")
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+}
